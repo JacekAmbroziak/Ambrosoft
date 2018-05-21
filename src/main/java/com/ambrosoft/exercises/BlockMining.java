6293a14402e7b41837f38db52070ea7a3cd27eb6
Jacek Ambroziak <jacek.ambroziak@gmail.com>
2018-05-21 18:52:47 -0400

    added sha256 to block mining

--- src/main/java/com/ambrosoft/exercises/BlockMining.java
+++ src/main/java/com/ambrosoft/exercises/BlockMining.java
@@ -1,5 +1,8 @@
 package com.ambrosoft.exercises;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
@@ -22,13 +25,20 @@ public class BlockMining {
 
         @Override
         public void run() {
-            while (GREENLIGHT) {
-                long number = ThreadLocalRandom.current().nextLong();
-                System.out.println("serial = " + serial + "\t" + number);
-                if (number > 0L && number < Long.MAX_VALUE / 2000L) {
-                    GREENLIGHT = false;
-                    System.out.println(serial + " found " + number);
+            try {
+                final MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+                while (GREENLIGHT) {
+                    final long number = ThreadLocalRandom.current().nextLong();
+                    final byte[] result = sha256.digest(Long.toHexString(number).getBytes());
+//                    System.out.println(Arrays.toString(result));
+                    if (result[0] == 0 && result[1] == 0 && result[2] == 0) {
+                        GREENLIGHT = false;
+                        System.out.println(serial + " found " + Arrays.toString(result));
+                        System.out.println("serial = " + serial + "\t" + number);
+                    }
                 }
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
             }
         }
     }
